6427a3448e5bb3d21ddf606fe443d4f63a9d80d8
marctex <marcelo.t.martins@hotmail.com>
2022-07-05 10:21:07 -0300

    continuidade no projeto

---
+++ unityifmsshootup/Assets/Scripts/Createenemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Createenemy : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public Vector3 range;
+    public float repeatTime;
+    void Start()
+    {
+        InvokeRepeating("Create", 0, repeatTime);       //chama o metodo Create para iniciar com 0segundos e repetir pelo valor de repeatTime
+    }
+
+    void Create()
+    {
+        GameObject prefab = prefabs[Random.Range(0, prefabs.Length)];     // cria prefab   a partir do array
+        Vector3 pos = this.transform.position + Vector3.Lerp(-range, range, Random.value);
+        Instantiate(prefab, pos, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
